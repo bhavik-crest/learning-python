d24f870c47b1ede76d3572dbf4c5ad22ec0f76eb
bhavik-crest <154506549+bhavik-crest@users.noreply.github.com>
2025-07-04 15:21:26 +0530

    Update database url

--- fast-api/config/db.py
+++ fast-api/config/db.py
@@ -1,4 +1,4 @@
 from pymongo import MongoClient
-MONGO_URI = "mongodb+srv://bhavikt:hs8sayh7vOyXzTkB@cluster0.8hoyu.mongodb.net"
+MONGO_URI = "mongodb+srv://username_password@cluster0.8hoyu.mongodb.net"
 
 conn = MongoClient(MONGO_URI)
