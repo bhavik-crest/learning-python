6f7db45fbc75fd477eb61b2419f360eb3ef88e23
bhavik-crest <154506549+bhavik-crest@users.noreply.github.com>
2025-07-04 15:19:36 +0530

    Update database URL

--- fast-api/main.py
+++ fast-api/main.py
@@ -5,7 +5,7 @@ from pymongo import MongoClient
 
 app = FastAPI()
 
-conn = MongoClient("mongodb+srv://bhavikt:hs8sayh7vOyXzTkB@cluster0.8hoyu.mongodb.net")
+conn = MongoClient("mongodb+srv://username_password@cluster0.8hoyu.mongodb.net")
 
 # @app.get("/")
 # def read_root():
