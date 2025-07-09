9bbe00cf876e1a5e49e02194f6765ebc9807194f
bhavik-crest <154506549+bhavik-crest@users.noreply.github.com>
2025-07-09 14:34:11 +0530

    Show phone number colum in admin site

--- django/my_tennis_club/members/admin.py
+++ django/my_tennis_club/members/admin.py
@@ -3,6 +3,6 @@ from .models import Member
 
 # Register your models here.
 class MemberAdmin(admin.ModelAdmin):
-    list_display = ("firstname", "lastname", "joined_date")
+    list_display = ("firstname", "lastname", "phone", "joined_date")
     
 admin.site.register(Member, MemberAdmin)
