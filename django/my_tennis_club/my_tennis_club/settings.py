b1bcd505e8b97a49bdd634d20d1933df34ea2e19
bhavik-crest <154506549+bhavik-crest@users.noreply.github.com>
2025-07-15 09:29:37 +0530

    Manage static files and update pages UI

--- django/my_tennis_club/my_tennis_club/settings.py
+++ django/my_tennis_club/my_tennis_club/settings.py
@@ -119,9 +119,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'productionfiles'
+
 STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Add this in your settings.py file:
+STATICFILES_DIRS = [
+    BASE_DIR / 'mystaticfiles'
+]
