9a35be3b7398bc7e805b3808c313ed7b67ffeae4
bhavik-crest <154506549+bhavik-crest@users.noreply.github.com>
2025-10-10 11:29:33 +0530

    Manage book scrapping with & without pagination

--- python-web-scraper/scraper.py
+++ python-web-scraper/scraper.py
@@ -30,7 +30,7 @@ except requests.exceptions.RequestException as err:
 
 
 
-#################################################### Books ####################################################
+#################################################### Books without pagination ####################################################
 # Step 1: Make a request to the page
 url = "https://books.toscrape.com"
 
@@ -55,10 +55,56 @@ try:
         image_url = f"{url}/{image}"
 
         books_data.append({
-            'title': title,
-            'price': price,
-            'image': image_url
+            "title": title,
+            "price": price,
+            "image": image_url
         })
+
+    print(books_data)
+
+except requests.exceptions.HTTPError as http_err:
+    print(f"For Books : HTTP error occurred: {http_err}")
+except requests.exceptions.ConnectionError:
+    print("For Books : Error connecting. Please check your network.")
+except requests.exceptions.Timeout:
+    print("For Books : Timeout error: The request took too long to complete.")
+except requests.exceptions.RequestException as err:
+    print(f"For Books : An unexpected error occurred: {err}")
+
+
+#################################################### Books with pagination ####################################################
+base_url = "https://books.toscrape.com"
+page_url = base_url
+
+books_data = []
+
+try:
+    while True:
+        response = requests.get(page_url)
+        response.raise_for_status()
+        bookList = BeautifulSoup(response.text, 'html.parser')
+        books = bookList.find_all('article', class_='product_pod')
+
+        for book in books:
+            title = book.h3.a['title']
+            price = book.find('p', class_='price_color').text
+            image = book.find('div', class_='image_container').a.img['src']
+            
+            # Use urljoin for robust image URL construction
+            image_url = urljoin(base_url, image)
+            books_data.append({
+                "title": title,
+                "price": price,
+                "image": image_url
+            })
+
+        # Find the next page link, if any
+        next_link = bookList.find('li', class_='next')
+        if next_link:
+            next_href = next_link.a['href']
+            page_url = urljoin(page_url, next_href)
+        else:
+            break  # No more pages; exit loop
 
     print(books_data)
 
